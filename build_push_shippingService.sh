3fdcf844b3948ef8bbfef81e551955d310762afb
Achmad Rofiqi Rapsanjani <90486402+rofiqirapsanjani@users.noreply.github.com>
2024-05-08 16:53:41 +0700

    Update and rename build_push_orderService.sh to build_push_shippingService.sh

--- build_push_shippingService.sh
+++ build_push_shippingService.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
   
 # Build Docker image
-docker build -t order-service:v1 .
+docker build -t shipping-service:v1 .
 
 # Display the list of local Docker images
 docker images
 
 # Tag the image according to Docker Hub format
-docker tag order-service:v1 sanjanivicky/order-service:v1
+docker tag order-service:v1 sanjanivicky/shipping-service:v1
 
 echo $PASSWORD_DOCKER_HUB | docker login -u sanjanivicky --password-stdin
 
 # Push the image to Docker Hub
-docker push sanjanivicky/order-service:v1
+docker push sanjanivicky/shipping-service:v1
